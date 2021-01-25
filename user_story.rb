83965b559a6fb50653f405d3c2288f404ae4d487
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-01-25 14:33:51 +0000

    user story and diagram created

---
+++ user_story.rb
@@ -0,0 +1,5 @@
+As a user
+So I can track of websites
+I would like to show a list of bookmarks
+
+doamin model diagram -> https://github.com/makersacademy/course/blob/master/bookmark_manager/images/bookmark_manager_1.png
