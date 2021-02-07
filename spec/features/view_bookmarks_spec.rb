917fad6675d9799f9286c384f899d2f647c33202
jshields123 <jackshields@live.co.uk>
2021-02-07 17:33:23 +0000

    refactored viewing bookmarks spec with create method

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -2,11 +2,9 @@ require 'pg'
 
 feature 'Viewing bookmarks' do
   scenario 'a user can see bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    connection.exec("INSERT INTO bookmarks VALUES (1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES (2, 'http://www.facebook.com');")
-    connection.exec("INSERT INTO bookmarks VALUES (3, 'http://www.twitter.com');")
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.facebook.com")
+    Bookmark.create(url: "http://www.twitter.com")
 
     visit('/bookmarks')
 
