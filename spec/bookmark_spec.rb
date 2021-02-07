6a6cab8dd307edd4d130a064ddd87836f00387e1
jshields123 <jackshields@live.co.uk>
2021-02-07 17:31:02 +0000

    refactored create bookmark method

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -18,4 +18,12 @@ describe Bookmark do
 
   end
   end
+  describe '.create' do
+    it 'creates a new bookmark' do
+      Bookmark.create(url: 'http://www.testbookmark.com')
+
+      expect(Bookmark.all).to include 'http://www.testbookmark.com'
+    end
+  end
+
 end
